214fbeb0e7ee1a32a6f620c4ff1994e7c460171c
irbaiev <a.irbaeiv75@gmail.com>
2023-10-21 17:32:24 +0300

    add vercel json files

--- chat/settings.py
+++ chat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-w(l%2w&gplp!u+=d)ctev@grq*25k@@_qwcwzmq*e90l2ufepc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -127,7 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
